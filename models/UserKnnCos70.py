15ff83cd8e0e4c4356829c8c9d0bf409a262c91c
toharhymes <anton.chana@gmail.com>
2023-11-28 14:53:23 +0100

    probable fix of absent pickle model

--- models/UserKnnCos70.py
+++ models/UserKnnCos70.py
@@ -23,9 +23,15 @@ class CustomUnpickler(pickle.Unpickler):
 class UserKnnCos70(pickle.Unpickler):
     def __init__(self):
         # Load the model when initializing the class
-        with open(USERKNNCOS70_NAME, "rb") as file:
-            self.model = CustomUnpickler(file).load()
-        self.data_for_predict = Dataset.construct(read_kion_dataset(fast_check=1, data_dir=DATA_DIR)["interactions"].df)
+        if os.path.exists(USERKNNCOS70_NAME):
+            with open(USERKNNCOS70_NAME, "rb") as file:
+                self.model = CustomUnpickler(file).load()
+            self.data_for_predict = Dataset.construct(
+                read_kion_dataset(fast_check=1, data_dir=DATA_DIR)["interactions"].df
+            )
+        else:
+            self.model = None
+            self.data_for_predict = None
 
     def predict(self, user_id):
         # Assuming data_for_predict is available as a class attribute
